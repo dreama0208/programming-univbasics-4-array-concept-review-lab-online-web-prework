690b49a3a675db7489d11f79dfbf9635f540ae65
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-03 19:00:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -23,8 +23,8 @@ end
 
 def find_min_value(array)
 min = 0
-  array.length-1.times { |index|
-    if array[index] < array[index+1]
+  array.length.times { |index|
+    if array[index] < array[index-1]
       min = array [index]
     end
   }
