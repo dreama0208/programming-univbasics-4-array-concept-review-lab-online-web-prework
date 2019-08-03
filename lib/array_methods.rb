1567e2244d9844fb0d25eb327c6e4f1aecb7397c
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-03 18:01:05 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,8 +10,9 @@ end
 def find_max_value(array)
   max = array[0]
   array.length.times {|index|
-  while array[index+1]>=array[index]
+  if array[index+1]>=array[index]
     max = array[index+1]
+  end
   }
   max
 end
