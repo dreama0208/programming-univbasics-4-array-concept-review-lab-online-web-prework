9804a7904960af7919908a2a09c0b209542efa1f
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-03 17:25:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,11 +1,16 @@
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  array.length.times { |index|
+  if condition
+    array[index]==value_to_find
+    index
+  end
+  }
 end
 
 def find_max_value(array)
-  # Add your solution here
+  array.max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  array.min
 end
