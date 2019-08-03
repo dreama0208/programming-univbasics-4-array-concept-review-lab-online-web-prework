5203a340474020cafdbfeb5d36f0012477b667b1
imperative-genre-4923 <dreama.lin@yale.edu>
2019-08-03 17:35:11 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,8 @@
 def find_element_index(array, value_to_find)
   array.length.times { |index|
-  if array[index]==value_to_find
+    while array[index]!=value_to_find
+    return nil
+  else
     return index
   end
   }
